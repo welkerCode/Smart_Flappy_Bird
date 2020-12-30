240e0a33cee4ac9ec7cfe015b7f2a1d25a77a94d
Taylor Welker <taylormaxwelker@hotmail.com>
2020-12-29 21:44:29 -0700

    Drawing first flappy bird and rough pipe

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 
+game_width = 200
+game_height = 300
 white = (255, 255, 255)
 black = (0, 0, 0)
 red = (255, 0, 0)
@@ -9,11 +11,13 @@ blue = (0, 0, 255)
 yellow = (255, 255, 0)
 
 game_over = False
-
+flappy_bird_im = pygame.image.load(r'C:\Users\taylo\source\repos\Smart_Flappy_Bird\images\flappy_bird.png')
+pipe_downward_im = pygame.image.load(r'C:\Users\taylo\source\repos\Smart_Flappy_Bird\images\pipe_downward.png')
 pygame.init()
-dis = pygame.display.set_mode((400,300))
+dis = pygame.display.set_mode((game_width, game_height))
 dis.fill(blue)
-pygame.draw.rect(dis, yellow, [10, 100, 30, 30])
+dis.blit(flappy_bird_im, (0, 0))
+dis.blit(pipe_downward_im, (50, -200))
 
 pygame.display.update()
 
