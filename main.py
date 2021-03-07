0b6307d644907fe78933be12fb323a77f6aa92c1
Taylor Welker <taylormaxwelker@hotmail.com>
2021-03-07 15:18:53 -0700

    Game crashes when bird touches ground.

--- main.py
+++ main.py
@@ -37,26 +37,31 @@ delay = 10000000
 count = 0
 while game_over is False:
     count += 1
-    if count > delay:
-        game_over = True
+
+    # Check for game over
     if count % 100000:
         bird_pos_y = bird_pos_y + 1
+
+        # If we touch the ground, end the game
         if bird_pos_y > bird_lowest_pos:
-            bird_pos_y = 0
+            game_over = True
 
-    # Define a frame
-    dis = pygame.display.set_mode((game_width, game_height))
+        # Otherwise, update the screen
+        else:
+    
+            # Define a frame
+            dis = pygame.display.set_mode((game_width, game_height))
 
-    # Draw the frame
-    dis.fill(blue)                                              # Fill in the background
-    dis.blit(flappy_bird_im, (bird_pos_x, bird_pos_y))          # Add the flappy bird
-    dis.blit(pipe_downward_im, (50, -200))                      # Add the downward pipe
-    dis.blit(pipe_upward_im, (100, 0))                          # Add the upward pipe
-    pygame.draw.rect(dis, green, grass)                         # Add the grass
-    pygame.draw.rect(dis, brown, dirt)                          # Add the dirt under the grass
+            # Draw the frame
+            dis.fill(blue)                                              # Fill in the background
+            dis.blit(flappy_bird_im, (bird_pos_x, bird_pos_y))          # Add the flappy bird
+            dis.blit(pipe_downward_im, (50, -200))                      # Add the downward pipe
+            dis.blit(pipe_upward_im, (100, 0))                          # Add the upward pipe
+            pygame.draw.rect(dis, green, grass)                         # Add the grass
+            pygame.draw.rect(dis, brown, dirt)                          # Add the dirt under the grass
 
-    # Update the frame
-    pygame.display.update()
+            # Update the frame
+            pygame.display.update()
 
 pygame.quit()
 quit()
